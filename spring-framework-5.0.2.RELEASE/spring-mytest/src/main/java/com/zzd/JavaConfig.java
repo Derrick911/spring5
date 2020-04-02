1b77e28ff9401415519e9699fb848af9bb175f51
张振栋 <zzd@spacetech.com.cn>
2020-04-02 14:34:09 +0800

    spring-zzd

---
+++ spring-framework-5.0.2.RELEASE/spring-mytest/src/main/java/com/zzd/JavaConfig.java
@@ -0,0 +1,15 @@
+package com.zzd;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class JavaConfig {
+
+	@Bean
+	public User user(){
+		return new User(101,"ganquanzhong","pwd","13995978321","china");
+	}
+}
---
+++ spring-framework-5.0.2.RELEASE/spring-mytest/src/main/java/com/zzd/Main.java
@@ -0,0 +1,18 @@
+package com.zzd;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+
+	public static void main(String[] args){
+
+		ApplicationContext ac =new AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User) ac.getBean("user");
+		System.out.println(user.toString());
+	}
+
+
+
+}
